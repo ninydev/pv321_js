0d8c0a12474f6e0c3347514f4524fdf1ce9e20cc
Oleksandr Nykytin <keeper@ninydev.com>
2024-06-20 20:43:05 +0300

    Нова Пошта

--- 2024_06_19/novaposhtaFetch.js
+++ 2024_06_19/novaposhtaFetch.js
@@ -12,14 +12,23 @@ function npFetchCallBack(calledMethod, methodProperties, callback) {
     })
         .then(res => res.json())
         .then(jsonResponse => {
+            console.log(jsonResponse)
             if (!jsonResponse.success) {
                 throw  { message : 'Data Error '}
             }
+
+            console.log(jsonResponse)
+
+            if(jsonResponse.info.totalCount)
+                myToastify("Get records:" + jsonResponse.info.totalCount)
 
             callback(jsonResponse.data)
 
         })
         .catch(err => {
+            myToastify("Error" + err.message, {
+                background: "linear-gradient(to right, #550000, #ff0000)"
+            })
             console.error('Error')
             console.error(err)
         })
