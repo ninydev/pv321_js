42721e874dcc4f28417002a7647c43bd4d75371b
Oleksandr Nykytin <keeper@ninydev.com>
2024-06-20 21:37:35 +0300

    Нова Пошта

--- 2024_06_19/MyLocalStorage.js
+++ 2024_06_19/MyLocalStorage.js
@@ -9,10 +9,14 @@ const MyLocalStorage = {
     set: (key, val, ttl) => {
         key = convertToJson(key)
         val = convertToJson(val)
+        // Зберегти ttl - окремо
         localStorage.setItem(key, val)
     },
 
     get: (key) => {
+        // Спочатку прочитати ttl
+        // та якщо зминна ще актуальна - повернути зминну
+        // якщо ні - видалити данні та ттл та мовернути нулл
         key = convertToJson(key)
         let result = localStorage.getItem(key)
         try {
@@ -23,6 +27,7 @@ const MyLocalStorage = {
     },
     remove: (key) => {
         key = convertToJson(key)
+        // Видалити ттл
         localStorage.removeItem(key)
     }
 
